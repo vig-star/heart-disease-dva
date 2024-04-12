892487534468fec32461bd0e9dd275be25ae171d
Vignesh Sreedhar <vigneshsreedhar2002@gmail.com>
2024-04-12 19:48:43 -0400

    incorporate model

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # heart-disease-dva
 
-## Running the app
+## Running the frontend
 
 On the root directory, run the Python server:
 
@@ -9,6 +9,25 @@ python -m http.server
 ```
 
 This will run a local server on port 8000 where you can access the application. Go on `localhost:8000` and access the pages you wish to view.
+
+## Running the backend Flask server
+
+Install the following packages and versions using `conda` (if you already have them, please remove them before using `conda remove <package>`):
+
+```
+conda install scikit-learn=1.2.
+conda install conda-forge::joblib
+conda install pandas=2.0.3
+conda install flask
+```
+
+On the `model/` directory, run the Python Flask server:
+
+```
+flask run --no-debugger
+```
+
+This will run a local Flask server on port 5000. Make sure to have it running for model results.
 
 ## Dataset
 
---
+++ model/rf_model.py
@@ -0,0 +1,51 @@
+from flask import Flask, request, jsonify
+import joblib
+import pandas
+from sklearn.preprocessing import LabelEncoder
+
+app = Flask(__name__)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        # get data
+        raw = pandas.read_csv("../data/heart_2022_no_nans.csv")
+        unimportant_features = ["RemovedTeeth", "DeafOrHardOfHearing", "BlindOrVisionDifficulty", "DifficultyDressingBathing", \
+                        "DifficultyErrands", "DifficultyWalking"]
+        raw = raw.drop(unimportant_features, axis=1)
+        encoder = LabelEncoder()
+        X = raw.drop("HadHeartAttack", axis=1)
+
+        # make sure to put a dictionary/object mapping X column to raw value in record
+        input_data = request.json['record']
+        if "HadHeartAttack" in input_data:
+            del input_data["HadHeartAttack"]
+        for feature in unimportant_features:
+            if feature in input_data:
+                del input_data[feature]
+
+        # encode record by fitting to X data
+        for i in X.columns:
+            encoder = LabelEncoder()
+            encoder.fit(X[i])
+            input_data[i] = encoder.transform([input_data[i]])[0]
+        
+        print(input_data)
+        
+        # create final test record
+        y_test = []
+        for i in X.columns:
+            y_test.append(input_data[i])
+        
+        print(y_test)
+        # make predictions using data
+        model = joblib.load('random_forest.joblib')
+        prediction = model.predict([y_test])[0]
+        # return the prediction as a JSON response and cast int64 to int
+        return jsonify({'prediction': int(prediction)}), 200
+    except Exception as e:
+        # handle any errors
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
